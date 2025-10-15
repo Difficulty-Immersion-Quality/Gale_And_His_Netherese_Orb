658752adb4378ab41d569fbd3dc046e01090a111
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2025-10-15 16:13:31 +1100

    Revised SE removal with ORI_GALE_BOMBDISARMED tag

--- Gale and His Netherese Orb - Devouring Allergy Edition/Mods/Gale and His Netherese Orb/ScriptExtender/Lua/Server/_Core.lua
+++ Gale and His Netherese Orb - Devouring Allergy Edition/Mods/Gale and His Netherese Orb/ScriptExtender/Lua/Server/_Core.lua
@@ -1,27 +1,19 @@
 local Gale = "S_Player_Gale_ad9af97d-75da-406a-ae13-7071c563f604"
-local BombDisarmedFlag = "ORI_Gale_Event_BombDisarmed_3d014e79-5595-9365-87bb-5cbb1f87fe5c"
+local DoneWithItemsTag = "ORI_GALE_BOMBDISARMED_f37724b5-9235-4ab2-8d65-78efcf50f95b"
 local StatusToRemove = "GOON_GALE_MAGICAL_APPETITE_UNLOCK"
 
--- Check on gameplay start
-Ext.Osiris.RegisterListener("FlagLoadedInPresetEvent", 2, "after", function(object, flag)
-    if object == Gale and flag == BombDisarmedFlag then
+-- On gameplay start, check if Gale already has the tag
+Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(levelName, isEditorMode)
+    if Osi.IsTagged(Gale, DoneWithItemsTag) == 1 then
         Osi.RemoveStatus(Gale, StatusToRemove)
-        print("[Gale and His Netherese Orb - A Magical Appetite] ORI_Gale_Event_BombDisarmed flag set. Gale is no longer hungry, status cleared:", StatusToRemove)
+        print("[Gale and His Netherese Orb - A Magical Appetite] ORI_GALE_BOMBDISARMED tag found, Gale is no longer hungry, status cleared:", StatusToRemove)
     end
 end)
 
--- Check when ORI_GALE_BOMB3 status is removed
--- Ext.Osiris.RegisterListener("StatusRemoved", 2, "after", function(character, status)
---     if status == "ORI_GALE_BOMB3" and character == Gale then
---         Osi.RemoveStatus(Gale, StatusToRemove)
---         print("[Gale and His Netherese Orb - A Magical Appetite] ORI_GALE_BOMB3 status removed. Gale is no longer hungry, status cleared:", StatusToRemove)
---     end
--- end)
-
--- React if the bomb disarmed flag gets set mid-game
-Ext.Osiris.RegisterListener("FlagSet", 3, "after", function(flag, speaker, dialogInstance)
-    if flag == BombDisarmedFlag and speaker == Gale then
+-- React dynamically if the tag gets added during play
+Ext.Osiris.RegisterListener("TagSet", 2, "after", function(object, tag)
+    if object == Gale and tag == DoneWithItemsTag then
         Osi.RemoveStatus(Gale, StatusToRemove)
-        print("[Gale and His Netherese Orb - A Magical Appetite] ORI_Gale_Event_BombDisarmed flag set. Gale is no longer hungry, status cleared:", StatusToRemove)
+        print("[Gale and His Netherese Orb - A Magical Appetite] ORI_GALE_BOMBDISARMED tag found, Gale is no longer hungry, status cleared:", StatusToRemove)
     end
 end)
